963800fca5eef61fcc75a8ca3ee774457fc67a0e
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-10-30 13:01:00 +0530

    commit

---
+++ seller-lab/footer.php
@@ -0,0 +1,24 @@
+
+    <!-- start js include path -->
+    <script src="js/jquery.min.js" ></script>
+    <!-- bootstrap -->
+    <script src="js/bootstrap.min.js" ></script>
+    <script src="js/bootstrapValidator.min.js" ></script>
+    <!-- counterup -->
+	<!-- data tables -->
+    <script src="js/jquery.dataTables.min.js" ></script>
+ 	<script src="js/dataTables.bootstrap4.min.js" ></script>
+    <script src="js/table_data.js" ></script>
+    <!-- Common js-->
+    <!-- Common js-->
+	<script src="js/app.js" ></script>
+    <script src="js/layout.js" ></script>
+    <!-- material -->
+    <script src="assets/material/material.min.js"></script>
+	<script src="js/Chart.bundle.js" ></script>
+    <script src="js/utils.js" ></script>
+    <script src="js/home-data2.js" ></script>
+    
+  </body>
+
+</html>
---
+++ seller-lab/profile.php
@@ -0,0 +1,61 @@
+<?php include('header.php'); ?>
+<?php include('sidebar.php'); ?>
+<div class="page-content-wrapper">
+    <div class="page-content">
+
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <div class="card card-topline-aqua">
+                    <div class="card-head">
+                        <header>My Profile</header>
+                        <a href="edit-profile.php" class="btn btn-primary btn-sm pull-right mt-2 mr-4">Edit</a>
+                    </div>
+                    <div class="card-body ">
+                        <div class="table-responsive">
+                            <table id="" class="table table-bordered" style="width:100%;">
+                                <tbody>
+                                    <tr>
+                                        <td>Name</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Mobile Number</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Email Id</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Address1</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Address2</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                    <tr>
+                                        <td>City</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                    <tr>
+                                        <td>State</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Pincode</td>
+                                        <td>xxxxx</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="clearfix">&nbsp;</div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+</div>
+<?php include('footer.php'); ?>
